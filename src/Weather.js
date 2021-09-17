9fba8af92d4ebb05a2541ae4c0a9f88d7856776f
Abigail Yi <abigail.yi@gmail.com>
2021-09-17 11:15:13 -0700

    Removed unused vars

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,5 @@
 import React from "react";
-import axios from "axios";
 import "./Weather.css";
-import Loader from "react-loader-spinner";
 
 export default function Weather() {
   let weatherData = {
