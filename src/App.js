4d1cd46c1803686ff72edd5b06b55b1e294f603b
Abigail Yi <abigail.yi@gmail.com>
2021-09-17 11:13:12 -0700

    Moved CodeSandbox React project to VS Code to deploy to Netlify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world!</h1>
-        <Weather city="Paris" />
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,70 @@
 import React from "react";
 import axios from "axios";
+import "./Weather.css";
 import Loader from "react-loader-spinner";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}ºC`
-    );
-  }
-  let apiKey = `1e900e7a532291ddab0851fd797f7887`;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&units=imperial&appid=${apiKey}`;
-  axios.get(url).then(handleResponse);
+export default function Weather() {
+  let weatherData = {
+    city: "Los Angeles",
+    temperature: 71,
+    date: "Monday 8:35 PM",
+    description: "Clear",
+    icon: "https://i.ibb.co/gMsgrCg/01n.png",
+    humidity: 32,
+    wind: 4,
+  };
 
   return (
-    <Loader
-      type="Puff"
-      color="#00BFFF"
-      height={100}
-      width={100}
-      timeout={3000} //3 secs
-    />
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search a city.."
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-115"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="weatherInfo">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temp float-left">
+            <img
+              src={weatherData.icon}
+              alt={weatherData.description}
+              className="float-left"
+            />
+          </div>
+          <div className="float-left weatherTemp">
+            <strong>{weatherData.temperature}</strong>
+            <span className="units">
+              <a href="/">ºC</a> | <a href="/">ºF</a>
+            </span>
+          </div>
+        </div>
+        <div className="col-6 weatherExtra">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
