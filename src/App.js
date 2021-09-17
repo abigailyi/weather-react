7ae0794d809966326d0ee61fb996203686514760
Abigail Yi <abigail.yi@gmail.com>
2021-09-17 11:43:55 -0700

    Imported Bootstrap and dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="container">
+        <Weather />
+        <footer className="mt-2">
+          This project was coded by{" "}
+          <a
+            href="https://eloquent-swartz-9918f7.netlify.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Abigail Yi
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/abigailyi/weather-react"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
